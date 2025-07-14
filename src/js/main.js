46fe99cecffe4142fded3b9c22b2b196e0e47123
Bogdan Kuzmenko <bkuzmenko83@mail.ru>
2025-07-15 03:05:05 +0500

    Переделана логика стилей: добавлены общие стили (для удобства и для лучшего вида при масштабировании страницы) в /style/base/scss для .header .nav .flex .grid и т.д; убран /btn; margin для .header теперь auto Реализован footer Начат forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 import '../scss/main.scss';
 import logoImage from '../images/ROBO.SCHOOL.png';
 import heroImage from '../images/grilnsircle.png';
+import procentImage from '../images/Mask-group.png'
